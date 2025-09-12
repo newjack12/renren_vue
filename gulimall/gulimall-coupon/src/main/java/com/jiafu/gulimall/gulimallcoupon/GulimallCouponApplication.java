aa5627f8e439eddf31592903071b9356a9e7e155
liu jiafu <2424235064@qq.com>
2025-09-12 11:36:19 +0200

    creat proj

---
+++ gulimall/gulimall-coupon/src/main/java/com/jiafu/gulimall/gulimallcoupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.jiafu.gulimall.gulimallcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+
+}
